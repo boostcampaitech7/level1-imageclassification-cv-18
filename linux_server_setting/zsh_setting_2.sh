3a8ada12fa785f109112035396490ecc8567f27c
chanHyeok-Leem <imcksgur@naver.com>
2024-09-13 19:23:15 +0000

    feat: zsh_settind_2 (custom & conda init)

--- linux_server_setting/zsh_setting_2.sh
+++ linux_server_setting/zsh_setting_2.sh
@@ -1,46 +1,43 @@
 #!/bin/zsh
 
-# 패키지 설치 (apt-get 수정 완료)
 apt-get install nano -y
 
-# ~/.zshrc 파일이 없으면 생성
-touch ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-# ZSH 테마 설정
 if grep -q '^ZSH_THEME=' ~/.zshrc; then
     sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="agnoster"/' ~/.zshrc
 else
     echo 'ZSH_THEME="agnoster"' >> ~/.zshrc
 fi
 
+echo '' >> ~/.zshrc 
+
+sed -i 's/^plugins=(.*)/plugins=(git zsh-syntax-highlighting)/' ~/.zshrc
+
+echo '' >> ~/.zshrc 
+
 # Git 브랜치 강조 설정
 if grep -q 'ZSH_THEME_GIT_PROMPT_PREFIX' ~/.zshrc; then
-    sed -i 's/^ZSH_THEME_GIT_PROMPT_PREFIX=.*$/ZSH_THEME_GIT_PROMPT_PREFIX="%F{cyan}("/' ~/.zshrc
-    sed -i 's/^ZSH_THEME_GIT_PROMPT_SUFFIX=.*$/ZSH_THEME_GIT_PROMPT_SUFFIX=")%f"/' ~/.zshrc
+    sed -i 's/^ZSH_THEME_GIT_PROMPT_PREFIX=.*$/ZSH_THEME_GIT_PROMPT_PREFIX="%F{magenta}["/' ~/.zshrc
+    sed -i 's/^ZSH_THEME_GIT_PROMPT_SUFFIX=.*$/ZSH_THEME_GIT_PROMPT_SUFFIX="]%f"/' ~/.zshrc
     sed -i 's/^ZSH_THEME_GIT_PROMPT_DIRTY=.*$/ZSH_THEME_GIT_PROMPT_DIRTY="%F{red}*%f"/' ~/.zshrc
     sed -i 's/^ZSH_THEME_GIT_PROMPT_CLEAN=.*$/ZSH_THEME_GIT_PROMPT_CLEAN=""/' ~/.zshrc
 else
-    echo 'ZSH_THEME_GIT_PROMPT_PREFIX="%F{cyan}("' >> ~/.zshrc
-    echo 'ZSH_THEME_GIT_PROMPT_SUFFIX=")%f"' >> ~/.zshrc
+    echo 'ZSH_THEME_GIT_PROMPT_PREFIX="%F{magenta}["' >> ~/.zshrc
+    echo 'ZSH_THEME_GIT_PROMPT_SUFFIX="]%f"' >> ~/.zshrc
     echo 'ZSH_THEME_GIT_PROMPT_DIRTY="%F{red}*%f"' >> ~/.zshrc
     echo 'ZSH_THEME_GIT_PROMPT_CLEAN=""' >> ~/.zshrc
 fi
 
-#프롬프트 설정
-if grep -q '^PROMPT=' ~/.zshrc; then
-    sed -i 's/^PROMPT=.*$/PROMPT="%F{yellow}%n@%m%f %F{blue}%~%f $(git_prompt_info)\n%F{green}→ %f"/' ~/.zshrc
-else
-    echo 'PROMPT="%F{yellow}%n@%m%f %F{blue}%~%f $(git_prompt_info)\n%F{green}→ %f"' >> ~/.zshrc
-fi
+echo '' >> ~/.zshrc 
 
-# PATH 설정
-if grep -q 'export PATH="/opt/conda/bin:$PATH"' ~/.zshrc; then
-    echo "PATH 설정이 이미 존재합니다."
-else
-    echo 'export PATH="/opt/conda/bin:$PATH"' >> ~/.zshrc
-fi
+sed -i '/^PROMPT=/,+1d' ~/.zshrc
 
-# Conda 활성화
+echo "PROMPT='%F{yellow}> %F{blue}%~%f $(git_prompt_info)
+%F{green}> %f'" >> ~/.zshrc
+
+echo '' >> ~/.zshrc 
+
 if [ -f /opt/conda/etc/profile.d/conda.sh ]; then
     source /opt/conda/etc/profile.d/conda.sh
 else
