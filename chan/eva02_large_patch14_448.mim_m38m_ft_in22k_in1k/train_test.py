a0e14c7360c48b92c0ed5de0aa9401da9a3336f4
ChanHyeok-Leem <imcksgur@naver.com>
2024-09-17 07:42:49 +0900

    fixes: model_name "/" error

--- chan/eva02_large_patch14_448.mim_m38m_ft_in22k_in1k/train_test.py
+++ chan/eva02_large_patch14_448.mim_m38m_ft_in22k_in1k/train_test.py
@@ -121,6 +121,8 @@ def train_test():
         pretrained= args.pretrained
     )
 
+    model_name = args.model_name.replace('/', '_')
+
     model = model_selector.get_model()
 
     # model 구조 모르겠으면 주석 풀고 확인
@@ -160,7 +162,7 @@ def train_test():
     weight_path= weight_dir,
     log_path= logfile,
     tensorboard_path= tensorboard_dir,
-    model_name = args.model_name,
+    model_name = model_name,
     pretrained = args.pretrained
     )
 
